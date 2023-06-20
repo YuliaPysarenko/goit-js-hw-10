cd8578f26f5a17c7c6d39afff0885b6247a8b79d
YuliaPysarenko <y.pisarenko@ukr.net>
2023-06-20 13:51:00 +0300

    homework

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
-// import CatApi from "./cat-api";
-// const catApi = new CatApi;
-
 import catApi from './cat-api';
 import linksDokQuerySel from './links';
 
 const link = linksDokQuerySel();
-
 link.breadSelect.addEventListener(`change`, onChangeSelectOption);
 
 catApi.fetchBreeds();
@@ -13,7 +9,5 @@ catApi.fetchBreeds();
 function onChangeSelectOption() {
     const selectedOptionValue = link.breadSelect.value;
        catApi.fetchCatByBreed(selectedOptionValue); 
-//    catApi.fetchCatByBreed(selectedOptionValue).then(renderCat); 
-    // catApi.fetchCatByBreed().then(renderCat);
 }   
 
